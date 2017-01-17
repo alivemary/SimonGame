0bc6fe46d6a72d692fb522ee7c1596a1d7aa6ecc
alivemary <alivemary@gmail.com>
2017-01-17 10:45:53 +0100

    Game can be on and off

--- js/simonGame.js
+++ js/simonGame.js
@@ -1,1 +1,22 @@
-﻿
+﻿$(document).ready(function () {
+    $('#gameWin').removeClass('hidden');
+    $('#loadWin').addClass('hidden');
+
+    $('.onoffswitch-label').click(function () {
+        //switch game status on/off
+        if ($('.onoffswitch-checkbox').is(':checked')) $('.myCheckbox').prop('checked', true);
+        else $('.onoffswitch-checkbox').prop('checked', false);
+        //if game on
+        if (!$('.onoffswitch-checkbox').is(':checked')) {
+            console.log("Game on!");
+            $('#count').html('-- ');
+        }
+            //if game off
+        else {
+            console.log("Game off!");
+            $('#count').html('');
+        }
+    });
+       
+   
+});
